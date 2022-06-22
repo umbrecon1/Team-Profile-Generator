f6588fd5a24020e0bd0a568ba498b79a17fbcb1f
Connor Mitchener <lightsoutconnor1@gmail.com>
2022-06-22 15:18:40 -0700

    add engineer

--- Main/index.js
+++ Main/index.js
@@ -110,3 +110,70 @@ function appMenu() {
         }
       });
   }
+
+  function addEngineer() {
+    inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'engineerName',
+          message: "What is your engineer's name?",
+          validate: (answer) => {
+            if (answer !== '') {
+              return true;
+            }
+            return 'Please enter at least one character.';
+          },
+        },
+        {
+          type: 'input',
+          name: 'engineerId',
+          message: "What is your engineer's id?",
+          validate: (answer) => {
+            const pass = answer.match(/^[1-9]\d*$/);
+            if (pass) {
+              if (idArray.includes(answer)) {
+                return 'This ID is already taken. Please enter a different number.';
+              } else {
+                return true;
+              }
+            }
+            return 'Please enter a positive number greater than zero.';
+          },
+        },
+        {
+          type: 'input',
+          name: 'engineerEmail',
+          message: "What is your engineer's email?",
+          validate: (answer) => {
+            const pass = answer.match(/\S+@\S+\.\S+/);
+            if (pass) {
+              return true;
+            }
+            return 'Please enter a valid email address.';
+          },
+        },
+        {
+          type: 'input',
+          name: 'engineerGithub',
+          message: "What is your engineer's GitHub username?",
+          validate: (answer) => {
+            if (answer !== '') {
+              return true;
+            }
+            return 'Please enter at least one character.';
+          },
+        },
+      ])
+      .then((answers) => {
+        const engineer = new Engineer(
+          answers.engineerName,
+          answers.engineerId,
+          answers.engineerEmail,
+          answers.engineerGithub
+        );
+        teamMembers.push(engineer);
+        idArray.push(answers.engineerId);
+        createTeam();
+      });
+  }
