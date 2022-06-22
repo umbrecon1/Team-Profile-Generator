98fb98e5997e2c96f6730eeaa992fc3ed3b32561
Connor Mitchener <lightsoutconnor1@gmail.com>
2022-06-22 15:14:43 -0700

    index variables

--- Main/index.js
+++ Main/index.js
@@ -1,1 +1,19 @@
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
 
+const DIST_DIR = path.resolve(__dirname, 'dist');
+const distPath = path.join(DIST_DIR, 'team.html');
+
+const render = require('./src/page-template.js');
+
+const teamMembers = [];
+const idArray = [];
+
+// Inform user of usage
+console.log(
+  '\nWelcome to the team generator!\nUse `npm run reset` to reset the dist/ folder\n'
+);
